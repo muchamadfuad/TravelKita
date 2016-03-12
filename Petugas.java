eb23f8258726b5f2b9c7744b7f5194b8f84fe326
lennisyarifah <lennisyarifah@yahoo.co.id>
2016-03-12 15:11:02 +0700

    Lenni Syarifah Lubis_ 1301144152

---
+++ Petugas.java
@@ -0,0 +1,20 @@
+public class Petugas extends Orang
+{
+	private String idPetugas;
+	private String Password;
+	
+	public Petugas(String nama,String noIdentitas,String alamat,String noHp,String idPetugas,String Password){
+		super(nama,noIdentitas,alamat,noHp);
+		this.idPetugas=idPetugas;
+		this.Password=Password;
+	}
+	public void setIdPetugas(String idPetugas){
+		this.idPetugas=idPetugas;
+	}
+	public String getIdPetugas(){
+		return this.idPetugas;
+	}
+	public void setPasswordPetugas(String Password){
+		this.Password=Password;
+	}
+}
